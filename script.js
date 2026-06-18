7c10740e908ea58535f1c40ed5c2aa20a9f41a04
moinkadivalmk4-tech <moinkadivalmk4@gmail.com>
2026-06-18 23:56:06 +0530

    Added basic board styling

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Kanban Board");
+const form = document.getElementById("taskForm");
+const input = document.getElementById("taskInput");
+const todo = document.getElementById("todo");
+
+form.addEventListener("submit",function(e){
+
+    e.preventDefault();
+
+    if(input.value.trim() === ""){
+        return;
+    }
+
+    const task = document.createElement("div");
+
+    task.textContent = input.value;
+
+    task.style.background = "#e8e8e8";
+    task.style.padding = "10px";
+    task.style.marginBottom = "10px";
+
+    todo.appendChild(task);
+
+    input.value = "";
+});
